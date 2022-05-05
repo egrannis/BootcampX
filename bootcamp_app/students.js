3396e325854fbc5478a07cbe7318dadd68334302
Emma Grannis <grannis.emma@gmail.com>
2022-05-05 22:40:37 +0000

    Refactor query to use parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,15 +10,16 @@ const pool = new Pool({
 // pool.query select is written as we would write SQL query
 // pool.query is a function that accepts an SQL query as a Javascript string. 
 // Using the ` backtick, we can write it multi-line
-// the function RETURNS A PROMISE that contains our result when the query is successful
+// the function RETURNS A PROMISE that contains our result when the query is successfu
 
-pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const text = `SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort 
+  FROM students 
+  JOIN cohorts ON cohort_id = cohorts.id 
+  WHERE cohorts.name LIKE $1 
+  LIMIT $2`
+const values = [process.argv[2], process.argv[3] || 5];
+
+pool.query(text, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
